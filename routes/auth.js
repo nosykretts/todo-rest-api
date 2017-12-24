011c59a722e62989ed4961899c139531dbd226b2
Fajar Patappari <top.sick.red@gmail.com>
2017-12-24 09:43:50 +0000

    update

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
 
 const {
-  signin
+  signin,
+  signup
 } = require('../controllers/auth')
 
 router.post('/signin', signin)
+router.post('/signup', signup)
 
 module.exports = router

ed08ab0a15a5bf3863d51a3b3aac00743dd7a6c9
Fajar Patappari <top.sick.red@gmail.com>
2017-12-29 19:55:34 +0000

    add user validation

--- models/user.js
+++ models/user.js
@@ -5,13 +5,16 @@ const Schema = mongoose.Schema
 let userSchema = new Schema(
   {
     name: {
-      type: String
+      type: String,
+      required: true
     },
     email: {
-      type: String
+      type: String,
+      required : true
     },
     password: {
       type: String,
+      required: true,
       select: false
     }
   },
